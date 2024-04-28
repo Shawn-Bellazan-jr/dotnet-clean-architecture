58a755b5be753e03d0e94d22a8ad1041309688bb
Shawn-Bellazan-jr <166971628+Shawn-Bellazan-jr@users.noreply.github.com>
2024-04-28 09:35:39 -0500

    feat(architecture): move clean architecture into src directory

---
+++ src/Example.Application/Common/Models/Result.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Example.Application.Common.Models
+{
+    public class Result
+    {
+        public bool Succeeded { get; init; } // Indicates whether the operation succeeded
+        public string[] Errors { get; init; } // Contains any error messages if the operation failed
+
+
+        // Internal constructor to initialize the Result object
+        internal Result(bool successed, IEnumerable<string> errors)
+        {
+            Succeeded = successed;
+            Errors = errors.ToArray();
+        }
+
+
+        // Static method to create a successful Result
+        public static Result Success()
+        {
+            return new Result(true, Array.Empty<string>());
+        }
+
+        // Static method to create a failed Result with error messages
+        public static Result Failure(IEnumerable<string> errors)
+        {
+           return new Result(false, errors);
+        }
+    }
+}
---
+++ src/Example.Domain/Common/BaseAuditableEntity.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Example.Domain.Common
+{
+    /// <summary>
+    ///  This class inherits from BaseEntity, which means it gains the domain event capabilities. 
+    ///  Auditable entities can also raise domain events when created or modified. 
+    /// </summary>
+    public abstract class BaseAuditableEntity : BaseEntity // Inherits from BaseEntity for domain event capabilities
+    {
+        /// <summary>
+        /// Each entity has a unique `Id` property, automatically generated as a GUID string.
+        /// </summary>
+        public DateTimeOffset Created { get; set; } // Timestamp when the entity was created
+        /// <summary>
+        /// This property stores information about the user who created the entity. This is often a username, user ID, or another identifier depending on your application's user management system. 
+        /// </summary>
+        public string? CreatedBy { get; set; } // Identifier (e.g., username) of the user who created the entity
+        /// <summary>
+        /// Similar to `Created`, this property tracks the date and time when the entity was last modified or updated. 
+        /// </summary>
+        public DateTimeOffset LastModified { get; set; } // Timestamp when the entity was last modified
+        /// <summary>
+        /// This property stores information about the user who last modified the entity, similar to `CreatedBy`.
+        /// </summary>
+        public string? LastModifiedBy { get; set; } // Identifier of the user who last modified the entity
+    }
+}
